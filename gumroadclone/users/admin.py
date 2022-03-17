852b7a9d5979dd9d2dce335b6755108a37fc6852
emnaruto07 <shazebvps@gmail.com>
2022-03-18 03:39:44 +0530

    UserLibrary model

--- gumroadclone/users/admin.py
+++ gumroadclone/users/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from .models import UserLibrary
 
 from gumroadclone.users.forms import UserAdminChangeForm, UserAdminCreationForm
 
 User = get_user_model()
 
+admin.site.register(UserLibrary)
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
