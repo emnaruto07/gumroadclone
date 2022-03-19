217e7259a4799c4cdf58acd8e65d675afa8904ae
emnaruto07 <shazebvps@gmail.com>
2022-03-19 20:03:58 +0530

    new_edit

--- gumroadclone/products/admin.py
+++ gumroadclone/products/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
-
-from .models import Product
+from gumroadclone.products.models import Product
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["name"]
